535566c511b83a9cb9def910a8d0a25d30262579
drftyguhi <gs1khamback@hotmail.com>
2024-05-08 21:05:35 +0700

    test project

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-function subsets(nums) {
-  const result = [];
-  backtrack(0, []);
-  return result;
-  function backtrack(start, current) {
-    result.push([...current]);
-    for (let i = start; i < nums.length; i++) {
-      current.push(nums[i]);
-      backtrack(i + 1, current);
-      current.pop();
+function uniquePaths(m, n) {
+  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
+  for (let i = 0; i < m; i++) {
+    dp[i][0] = 1;
+  }
+  for (let j = 0; j < n; j++) {
+    dp[0][j] = 1;
+  }
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
     }
   }
+  return dp[m - 1][n - 1];
 }
